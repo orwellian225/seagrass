229e4e45977fb95daf4027da7e0653e5d4c5f737
Orwellian <orwellian225@gmail.com>
2024-03-02 15:14:31 +0200

    remove: logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,9 @@
 <svelte:body class="w-screen min-h-screen h-fit bg-stone-50 flex flex-col justify-start items-center gap-10 p-2 overflow-y-auto overflow-x-hidden" />
 
 <header class="w-full h-fit flex flex-col md:flex-row gap-3 md:gap-20 border-b-4 border-rose-of-sharon-700 justify-start items-center md:py-2 md:px-5">
-    <a href="/" class="border-x-4 px-10 border-science-blue-600 rounded-md border-b-4 border-b-stone-50 hover:border-b-science-blue-600 flex flex-row gap-5 justify-center items-center">
+    <a href="/" class="border-x-4 px-10 border-science-blue-600 rounded-md border-y-4 border-y-stone-50 hover:border-b-science-blue-600 flex flex-row gap-5 justify-center items-center">
         <h1 class="text-5xl w-fit">SeaGrass</h1>
-        <img src="favicon.svg" width="64" height="64" alt="seagrass">
+        <!-- <img src="favicon.svg" width="64" height="64" alt="seagrass"> -->
     </a>
 
     <section class="w-full h-full flex flex-col justify-center items-center md:items-start px-2 md:px-0">
