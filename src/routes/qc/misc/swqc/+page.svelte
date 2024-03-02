774225e0e8dfb65bae999867a1ddbc4c64757c23
Orwellian <orwellian225@gmail.com>
2024-03-02 15:34:37 +0200

    add: annotated bibliography format

--- src/routes/qc/misc/swqc/+page.svelte
+++ src/routes/qc/misc/swqc/+page.svelte
@@ -1,4 +1,22 @@
-
 <article class="flex flex-col gap-5">
     <h3 class="text-3xl">Strengths and Weaknesses of Quantum Computing</h3>
+
+    <ul>
+        <li><i>Authors</i>: </li>
+        <li><i>Date</i>: </li>
+        <li><i>Link</i>: <a href="" class="underline text-rose-of-sharon-700 hover:text-rose-of-sharon-900"></a></li>
+        <li><i>Style</i>: </li>
+    </ul>
+
+    <section>
+        <h4 class="text-xl">Aim</h4>
+    </section>
+
+    <section>
+        <h4 class="text-xl">Cross Reference</h4>
+    </section>
+
+    <section>
+        <h4 class="text-xl">Summary</h4>
+    </section>
 </article>
--- src/routes/qc/misc/uqs/+page.svelte
+++ src/routes/qc/misc/uqs/+page.svelte
@@ -1,4 +1,22 @@
-
 <article class="flex flex-col gap-5">
     <h3 class="text-3xl">Universal Quantum Simulator</h3>
+
+    <ul>
+        <li><i>Authors</i>: Seth Lloyd</li>
+        <li><i>Date</i>: 23/08/1996</li>
+        <li><i>Link</i>: <a href="https://doi.org/10.1126/science.273.5278.1073" class="underline text-rose-of-sharon-700 hover:text-rose-of-sharon-900">Science.org</a></li>
+        <li><i>Style</i>: Journal Article, Theoretical</li>
+    </ul>
+
+    <section>
+        <h4 class="text-xl">Aim</h4>
+    </section>
+
+    <section>
+        <h4 class="text-xl">Cross Reference</h4>
+    </section>
+
+    <section>
+        <h4 class="text-xl">Summary</h4>
+    </section>
 </article>
