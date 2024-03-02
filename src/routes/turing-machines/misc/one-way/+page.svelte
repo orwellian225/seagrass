f24b8abc094ca1495e2e3345657fdc0ff32b344f
Orwellian <orwellian225@gmail.com>
2024-03-02 12:22:24 +0200

    add: Misc TM Pages

---
+++ src/routes/turing-machines/misc/one-way/+page.svelte
@@ -0,0 +1,4 @@
+<article class="p-2 flex justify-start items-start flex-col gap-2">
+    <h3 class="text-3xl">One-Way vs Two-Way Tapes</h3>
+
+</article>
---
+++ src/routes/turing-machines/misc/stationary-tapehead/+page.svelte
@@ -0,0 +1,4 @@
+<article class="p-2 flex justify-start items-start flex-col gap-2">
+    <h3 class="text-3xl">Stationary Movement equivalence with Left & Right Movement</h3>
+
+</article>
