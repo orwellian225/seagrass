3e0843020b5b4abc9b4daff7a1f0956685f8eb85
Orwellian <orwellian225@gmail.com>
2024-11-25 17:57:59 +0200

    add: crn page

--- src/routes/projects/computing-ramnums/+page.svelte
+++ src/routes/projects/computing-ramnums/+page.svelte
@@ -1,1 +1,42 @@
-<h3>Computing Ramsey Numbers</h3>
+<script lang="ts">
+    import InternalLink from "$lib/components/internal-link.svelte"
+
+</script>
+
+<div class="w-full md:w-2/3 h-full flex flex-col justify-start items-center ">
+    <h2 class='text-xl border-b-2'>Computing Ramsey Numbers</h2>
+
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Table of Contents</h3>
+
+        <ol class="list-decimal list-inside">
+            <li>Report: <InternalLink link="/Computing-Ramsey-Numbers.pdf">View</InternalLink></li>
+            <li>Poster: <InternalLink link="/CRN-Poster.pdf">View</InternalLink></li>
+            <li>Introduction</li>
+            <li>Graph Theory</li>
+            <li>Ramsey Theory</li>
+            <li>Ramsey Numbers as a Decision Problem</li>
+            <li>Classical Algorithms</li>
+            <li>Quantum Algorithms</li>
+        </ol>
+    </section>
+
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Introduction</h3>
+    </section>
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Graph Theory</h3>
+    </section>
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Ramsey Theory</h3>
+    </section>
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Ramsey Numbers as a Decision Problem</h3>
+    </section>
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Classical Algorithms</h3>
+    </section>
+    <section class="w-4/5 md:w-2/3 h-full flex flex-col justify-start items-start py-5 gap-5">
+        <h3 class="text-lg border-b-2">Quantum Algorithms</h3>
+    </section>
+</div>
